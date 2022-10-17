b20255e38d0f7e05ddbe73471fc3b6860a32b71f
VladimiraRe <vladimira_ra@mail.ru>
2022-10-17 23:06:55 +0300

    Started section about prices

--- src/js/section__swiper.js
+++ src/js/section__swiper.js
@@ -1,58 +1,79 @@
 import Swiper from './swiper-bundle.min2';
 
-const sliders = document.querySelectorAll('.swiper');
 var mySwipers = [];
-
-function sliderInit() {
-  sliders.forEach((slider, index) => {
-    if (!slider.swiper) {
-      mySwipers[index] = new Swiper('.swiper', {
-        direction: 'horizontal',
-        spaceBetween: 16,
-        slidesPerView: 1.185,
-        roundLengths: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-          360: {
-            slidesPerView: 1.35,
-          },
-          480: {
-            slidesPerView: 1.7,
-          },
-          576: {
-            slidesPerView: 2.3,
-          },
-          640: {
-            slidesPerView: 2.5,
-          },
-        }
-      });
-    } else {
-      return
-    }
-  })
-}
-
-function sliderDestroy() {
-  sliders.forEach((slider, index) => {
-    if (slider.swiper) {
-      mySwipers[0][index].destroy(true, true);
-    }
-  })
-}
+let isActive = false;
 
 function checker() {
-  if (window.innerWidth >= 768) {
-    sliderDestroy()
-  } else {
-    sliderInit()
+  if (!isActive && window.innerWidth < 768) {
+    sliderInit();
+    return
+  }
+  if (isActive && window.innerWidth >= 768) {
+    sliderDestroy();
+    return
   }
 }
 
 checker();
 
 window.addEventListener('resize', () => { checker() });
+
+function sliderInit() {
+    mySwipers = new Swiper('.section__swiper', {
+      direction: 'horizontal',
+      spaceBetween: 16,
+      slidesPerView: 1.185,
+      roundLengths: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        360: {
+          slidesPerView: 1.185 + 0.165,
+        },
+        480: {
+          slidesPerView: 1.185 + 0.515,
+        },
+        576: {
+          slidesPerView: 1.185 + 1.115,
+        },
+        640: {
+          slidesPerView: 1.185 + 1.315,
+        },
+      }
+    });
+    mySwipers[2] = new Swiper('.section__swiper--big', {
+      direction: 'horizontal',
+      spaceBetween: 16,
+      slidesPerView: 1.1,
+      roundLengths: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        360: {
+          slidesPerView: 1.1 + 0.165,
+        },
+        480: {
+          slidesPerView: 1.1 + 0.515,
+        },
+        576: {
+          slidesPerView: 1.1 + 1.115,
+        },
+        640: {
+          slidesPerView: 1.1 + 1.315,
+        },
+      }
+    });
+    isActive = true;
+}
+
+function sliderDestroy() {
+  mySwipers.forEach((swiper) => {
+    swiper.destroy(true, true);
+    isActive = false;
+  })
+}
 
