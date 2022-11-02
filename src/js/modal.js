a270f32cfc314be80c7e22648193fd24a9a896cb
VladimiraRe <vladimira_ra@mail.ru>
2022-11-02 22:31:04 +0300

    Restructuring html markup of modal section

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,103 +1,115 @@
-const headerModalBtns = document.querySelectorAll('.header__wrap:nth-child(2) .header__list:first-child .header__item:nth-child(-n + 2)');
-const modals = document.querySelectorAll('.modal');
-const container = document.querySelector('.container');
-const sidebar = document.querySelector('.sidebar');
-const sidebarModalBtns = sidebar.querySelectorAll('.sidebar__icons .sidebar__icons-item:nth-child(-n + 2)');
-const closeBtns = document.querySelectorAll('.modal .modal__close');
+const headerModalBtns = document.querySelectorAll(
+    '.header__wrap:nth-child(2) .header__list:first-child .header__item:nth-child(-n + 2)'
+)
+const modals = document.querySelectorAll('.modal')
+const modalWrap = document.querySelector('.modal__wrapper')
+const container = document.querySelector('.container')
+const sidebar = document.querySelector('.sidebar')
+const sidebarModalBtns = sidebar.querySelectorAll(
+    '.sidebar__icons .sidebar__icons-item:nth-child(-n + 2)'
+)
+const closeBtns = document.querySelectorAll('.modal .modal__close')
 
 let htmlObj, closeBtn
 
-let modalBtnsArray = Array.from(headerModalBtns);
+let modalBtnsArray = Array.from(headerModalBtns)
 sidebarModalBtns.forEach((i) => {
-    modalBtnsArray.push(i);
-});
+    modalBtnsArray.push(i)
+})
 
-checker();
-window.addEventListener('resize', () => { checker(); })
+checker()
+window.addEventListener('resize', () => {
+    checker()
+})
 
 function checker() {
     for (let i = 0; i < modalBtnsArray.length; i++) {
         function catchBtn(currentItem) {
-            let isSidebarOpen = false;
-            let item = currentItem;
+            let isSidebarOpen = false
+            let item = currentItem
 
             if (item >= modals.length) {
-                isSidebarOpen = true;
-                item = item - modals.length;
+                isSidebarOpen = true
+                item = item - modals.length
             }
 
-            if (modals[item].classList.contains('modal--open') &&
-            document.documentElement.clientWidth >= 768) {
-                
-                htmlObj = modals[item];
-                container.addEventListener('click', closeByClickOnContent);
+            if (
+                modals[item].classList.contains('modal--open') &&
+                document.documentElement.clientWidth >= 768
+            ) {
+                htmlObj = modals[item]
+                container.addEventListener('click', closeByClickOnContent)
 
                 if (document.documentElement.clientWidth >= 1440) {
-                    sidebar.classList.add('sidebar--locked');
+                    sidebar.classList.add('sidebar--locked')
                 }
             }
 
-            modalBtnsArray[currentItem].addEventListener('click', () => { openClose(isSidebarOpen, item) });
+            modalBtnsArray[currentItem].addEventListener('click', () => {
+                openClose(isSidebarOpen, item)
+            })
         }
 
-        catchBtn(i);
+        catchBtn(i)
     }
 }
 
 function openClose(isSidebarOpen, item) {
     if (isSidebarOpen) {
-        closeSidebar();
+        closeSidebar()
     }
 
-    htmlObj = modals[item];
-    closeBtn = closeBtns[item];
+    htmlObj = modals[item]
+    closeBtn = closeBtns[item]
 
-    openModal();
-    htmlObj.addEventListener('transitionend', closeModal, {once: true});
+    openModal()
+    modalWrap.addEventListener('transitionend', closeModal, { once: true })
 }
 
 function closeSidebar() {
     if (sidebar.classList.contains('sidebar--open')) {
-        sidebar.classList.add('sidebar--position');
+        sidebar.classList.add('sidebar--position')
         setTimeout(() => {
-            sidebar.classList.remove('sidebar--open');
+            sidebar.classList.remove('sidebar--open')
         }, 100)
     }
 }
 
 function openModal() {
     if (document.documentElement.clientWidth >= 1440) {
-        sidebar.classList.add('sidebar--locked');
+        sidebar.classList.add('sidebar--locked')
     }
 
-    container.classList.add('container--locked');
-    htmlObj.classList.add('modal--open');
+    container.classList.add('container--locked')
+    htmlObj.classList.add('modal--open')
+    modalWrap.classList.add('modal--open')
     setTimeout(() => {
-        htmlObj.classList.remove('modal--position');
+        modalWrap.classList.remove('modal__wrapper--position')
     }, 100)
 }
 
 function closeModal() {
-    closeBtn.addEventListener('click', close);
+    closeBtn.addEventListener('click', close)
     if (document.documentElement.clientWidth >= 768) {
-        container.addEventListener('click', closeByClickOnContent);
+        container.addEventListener('click', closeByClickOnContent)
     }
 }
 
 function closeByClickOnContent(evt) {
-    if(evt.clientX < document.documentElement.clientWidth - 440) {
-        close();
+    if (evt.clientX < document.documentElement.clientWidth - 440) {
+        close()
     }
 }
 
 function close() {
-    container.classList.remove('container--locked');
-    sidebar.classList.remove('sidebar--locked');
-    htmlObj.classList.add('modal--position');
+    container.classList.remove('container--locked')
+    sidebar.classList.remove('sidebar--locked')
+    modalWrap.classList.add('modal__wrapper--position')
     setTimeout(() => {
-        htmlObj.classList.remove('modal--open');
+        htmlObj.classList.remove('modal--open')
+        modalWrap.classList.remove('modal--open')
     }, 100)
 
-    closeBtn.removeEventListener('click', close);
-    container.removeEventListener('click', closeByClickOnContent);
+    closeBtn.removeEventListener('click', close)
+    container.removeEventListener('click', closeByClickOnContent)
 }
